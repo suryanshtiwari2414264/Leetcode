c18919a7f78c6da0dcfbbad1bab4ba6763abd50b
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-07-29 18:59:12 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,37 @@
+//ASSIGNING 0 TO THE VALUES;
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int n=nums.size();
+//         int i=0;
+//         int j=0;
+//         while(i<n){
+//             if(nums[i]!=0){
+//                 nums[j]=nums[i];
+//                 j++;
+//             }
+//             i++;
+//         }
+//         while(j<n){
+//             nums[j]=0;
+//             j++;
+//         }
+//     }
+// };
+
+// BY USING SWAPPING METHOD
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int i=0;
-        int j=0;
-        while(i<n){
-            if(nums[i]!=0){
-                nums[j]=nums[i];
+
+        int j = 0;   
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
                 j++;
             }
-            i++;
-        }
-        while(j<n){
-            nums[j]=0;
-            j++;
         }
     }
 };
