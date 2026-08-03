a0b7bcea15c074cc79f529c1d443cc536d5c6647
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-08-03 09:56:55 +0530

    Time: 0 ms (100%), Space: 15.1 MB (48.9%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -54,37 +54,25 @@ public:
         bool leftToRight = true;
 
         while (!q.empty()) {
-
             int size = q.size();
-
             vector<int> level(size);
-
             for (int i = 0; i < size; i++) {
-
                 TreeNode* node = q.front();
                 q.pop();
-
                 int index;
-
                 if (leftToRight)
                     index = i;
                 else
                     index = size - 1 - i;
-
                 level[index] = node->val;
-
                 if (node->left)
                     q.push(node->left);
-
                 if (node->right)
                     q.push(node->right);
             }
-
             ans.push_back(level);
-
             leftToRight = !leftToRight;
         }
-
         return ans;
     }
 };
