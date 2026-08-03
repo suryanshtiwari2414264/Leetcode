06f66f7ae3c047204cb55834a66a869ee141a9bc
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-08-03 10:00:54 +0530

    Time: 1 ms (15.67%), Space: 15.2 MB (48.9%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -19,6 +19,7 @@
 
 //     queue<TreeNode*> q;
 //     q.push(root);
+     // bool ltoR=true;
 
 //     while (!q.empty()) {
 //         int size = q.size();
@@ -39,6 +40,41 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+//         vector<vector<int>> ans;
+//         if (root == NULL)
+//             return ans;
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         bool leftToRight = true;
+//         while (!q.empty()) {
+//             int size = q.size();
+//             vector<int> level(size);
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode* node = q.front();
+//                 q.pop();
+//                 int index;
+//                 if (leftToRight)
+//                     index = i;
+//                 else
+//                     index = size - 1 - i;
+//                 level[index] = node->val;
+//                 if (node->left)
+//                     q.push(node->left);
+//                 if (node->right)
+//                     q.push(node->right);
+//             }
+//             ans.push_back(level);
+//             leftToRight = !leftToRight;
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -54,25 +90,34 @@ public:
         bool leftToRight = true;
 
         while (!q.empty()) {
+
             int size = q.size();
-            vector<int> level(size);
+            vector<int> level;
+
             for (int i = 0; i < size; i++) {
+
                 TreeNode* node = q.front();
                 q.pop();
-                int index;
-                if (leftToRight)
-                    index = i;
-                else
-                    index = size - 1 - i;
-                level[index] = node->val;
+
+                level.push_back(node->val);
+
                 if (node->left)
                     q.push(node->left);
+
                 if (node->right)
                     q.push(node->right);
             }
+
+            // Reverse the current level if needed
+            if (!leftToRight)
+                reverse(level.begin(), level.end());
+
             ans.push_back(level);
+
+            // Toggle direction
             leftToRight = !leftToRight;
         }
+
         return ans;
     }
 };
