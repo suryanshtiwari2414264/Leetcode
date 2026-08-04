45609ebd902393abf0b5cd6108866658293abecf
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-08-04 17:39:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.25%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return solve(x,N);
+    }
+private:
+    double solve(double x, long long n){
+        if(n==0)
+            return 1;
+        double half=solve(x,n/2);
+
+        if(n%2==0)
+            return half*half;
+        
+        return x*half*half;
+    }
+};
