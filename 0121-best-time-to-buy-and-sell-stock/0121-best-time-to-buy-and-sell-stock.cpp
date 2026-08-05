d39216fa8d75b56fd0ce54a15dd1f7c20b593631
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-08-05 17:46:17 +0530

    Time: 0 ms (100%), Space: 97.2 MB (88.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,37 @@
-class Solution {
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+
+//         int minPrice = prices[0];
+//         int maxProfit = 0;
+
+//         for(int i = 1; i < prices.size(); i++) {
+
+//             minPrice = min(minPrice, prices[i]);
+
+//             int profit = prices[i] - minPrice;
+
+//             maxProfit = max(maxProfit, profit);
+//         }
+
+//         return maxProfit;
+//     }
+// };
+
+//KADANE'S Solution
+class Solution{
 public:
-    int maxProfit(vector<int>& prices) {
-
-        int minPrice = prices[0];
-        int maxProfit = 0;
-
-        for(int i = 1; i < prices.size(); i++) {
-
-            minPrice = min(minPrice, prices[i]);
-
-            int profit = prices[i] - minPrice;
-
-            maxProfit = max(maxProfit, profit);
+    int maxProfit(vector<int> &prices){
+        int currsum=0;
+        int maxprof=0;
+        int n=prices.size();
+        for(int i=1;i<n;i++){
+            currsum += prices[i]-prices[i-1];
+            if(currsum<0)
+                currsum=0;
+            maxprof = max(maxprof,currsum);
         }
+        return maxprof;
 
-        return maxProfit;
     }
 };
