1ace6bcbfa51f0da8d6ff56a72c8acdf8696ae34
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-08-06 17:47:08 +0530

    Time: 146 ms (5.2%), Space: 113.6 MB (7.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,19 +1,38 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums)
+//     {
+//     int n=nums.size();
+//      sort(nums.begin(),nums.end());
+//      if(n==0){
+//         return false;
+//      }
+//      for(int i = 0;i<n-1;i++)
+//      {
+//         if(nums[i]==nums[i+1])
+//         {
+//             return true;
+//         }
+//      }
+//      return false; 
+//     }
+// };
+
+//AVL TREE
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums)
-    {
-    int n=nums.size();
-     sort(nums.begin(),nums.end());
-     if(n==0){
-        return false;
-     }
-     for(int i = 0;i<n-1;i++)
-     {
-        if(nums[i]==nums[i+1])
-        {
-            return true;
+    bool containsDuplicate(vector<int>& nums) {
+
+        set<int> st;
+
+        for(int num : nums){
+
+            if(st.find(num) != st.end())
+                return true;
+
+            st.insert(num);
         }
-     }
-     return false; 
+
+        return false;
     }
 };
