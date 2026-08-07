e694f3c18c060ec24f35c192aeaebeba8c270940
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-08-07 18:32:20 +0530

    Time: 0 ms (100%), Space: 21.1 MB (17.5%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,67 @@
+// BETTER APPROACH
+
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//          int m = matrix.size();
+//         int n = matrix[0].size();
+//         vector<int> row(m,0);
+//         vector<int> col(n,0);
+//         for(int i =0;i<m; i++){
+//             for(int j=0;j<n;j++){
+//                 if(matrix[i][j]==0){
+//                     row[i]=1;
+//                     col[j]=1;
+
+//                 }
+//             }
+//         }
+//         for(int i =0;i<m;i++){
+//             for(int j =0;j<n;j++){
+//                 if(row[i] || col[j])
+//                     matrix[i][j]=0;
+
+//             }
+//         }
+//     }
+// };
+
+// OPTIMAL SOLUTION
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-         int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> row(m,0);
-        vector<int> col(n,0);
-        for(int i =0;i<m; i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
 
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int col0 = 1;
+
+        for(int i=0;i<m;i++)
+        {
+            if(matrix[i][0]==0)
+                col0=0;
+
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(row[i] || col[j])
-                    matrix[i][j]=0;
 
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j=n-1;j>=1;j--)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
             }
+
+            if(col0==0)
+                matrix[i][0]=0;
         }
     }
 };
