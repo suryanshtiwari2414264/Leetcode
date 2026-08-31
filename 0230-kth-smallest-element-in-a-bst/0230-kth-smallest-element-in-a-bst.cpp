22fcef4c23d90ef9d58404a6d6e2f83de2ec6a08
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-08-31 11:44:45 +0530

    Time: 0 ms (100%), Space: 24.5 MB (27.94%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,19 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int> ans;
+//         inorder(root,ans);
+//         return ans[k-1];
+//     }
+//     void inorder(TreeNode* root, vector<int>&ans){
+//         if(root == NULL){
+//             return;//BASE CASE
+//         }
+//         inorder(root->left,ans);//LEFT
+//         ans.push_back(root->val);//NODE
+//         inorder(root->right,ans);//RIGHT
+//     }
+// };
+
+//2nd APPROACH
 class Solution {
 public:
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> ans;
-        inorder(root,ans);
-        return ans[k-1];
-    }
-    void inorder(TreeNode* root, vector<int>&ans){
-        if(root == NULL){
-            return;//BASE CASE
+    int kthSmallest(TreeNode* root, int k)
+    {
+        stack<TreeNode*>st;
+        TreeNode* curr= root;
+        while(curr!=NULL || !st.empty())
+        {
+            while(curr!=NULL){
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            k--;
+            if(k==0)
+                return curr->val;
+            curr = curr->right;
         }
-        inorder(root->left,ans);//LEFT
-        ans.push_back(root->val);//NODE
-        inorder(root->right,ans);//RIGHT
+        return -1;
     }
+
 };
+
+//3rd APPROACH
+// class Solution {
+// public:
+//     int kthSmallest(TreeNode* root, int k){
+        
+
+//     }
+
+// };
