6fe4af0eca11127cb98d42d6735b0e0fb220dcd3
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-09-01 15:03:43 +0530

    Time: 0 ms (100%), Space: 20.1 MB (45.52%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -8,13 +8,49 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ // RECURSIVE APPROACH
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         if(head == NULL)
+//             return NULL;
+//         head->next = removeElements(head->next,val);
+//         return head->val == val ? head->next : head;
+        
+//     }
+// };
+
+// 2nd Approach 
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
+
+        // Remove matching nodes from the beginning
+        while(head != NULL && head->val == val)
+        {
+            head = head->next;
+        }
+
         if(head == NULL)
             return NULL;
-        head->next = removeElements(head->next,val);
-        return head->val == val ? head->next : head;
-        
+
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+
+        while(curr != NULL)
+        {
+            if(curr->val == val)
+            {
+                prev->next = curr->next;
+            }
+            else
+            {
+                prev = curr;
+            }
+
+            curr = curr->next;
+        }
+
+        return head;
     }
 };
