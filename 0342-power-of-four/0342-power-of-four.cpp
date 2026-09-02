00e5a0f09451cc85ec80fe03e8de5e63c338edfe
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-09-02 15:47:21 +0530

    Time: 0 ms (100%), Space: 7.9 MB (48.94%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,22 @@
 //Iterative Approach
-class Solution {
+// class Solution {
+// public:
+//     bool isPowerOfFour(int n) {
+//         if(n<=0)
+//             return false;
+//         while(n%4==0)
+//             n = n/4;
+//         return n==1;
+        
+//     }
+// };
+
+//Recursive Approach
+class Solution{
 public:
-    bool isPowerOfFour(int n) {
+    bool isPowerOfFour(int n){
         if(n<=0)
             return false;
-        while(n%4==0)
-            n = n/4;
-        return n==1;
-        
+        return((n==1) || (n%4==0 && isPowerOfFour(n/4)));
     }
 };
