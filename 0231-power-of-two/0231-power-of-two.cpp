4ed3d453964d4052d27bdcd924185f8b0bf2f13a
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-09-02 14:07:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,18 @@
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if(n==0){
+//             return false;
+//         }
+//         while(n%2==0) n = n/2;
+//         return n==1;       
+//     }
+// };
+// USING RECURSION
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0){
-            return false;
-        }
-        while(n%2==0) n = n/2;
-        return n==1;
-
-        
+        if(n==0) return false;
+        return ((n==1) || (n%2==0 && isPowerOfTwo(n/2)));
     }
 };
