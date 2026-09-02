32c20d2104df56bf0a005e7110530eb0795f2575
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-09-02 15:43:05 +0530

    Time: 9 ms (9.62%), Space: 8.9 MB (75.1%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,10 +1,22 @@
-class Solution {
+//Iterative Approach
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if(n<=0)
+//             return false;
+//         while(n%3==0)
+//             n=n/3;
+//         return n==1;        
+//     }
+// };
+
+// Recursive Approach 
+class Solution{
 public:
-    bool isPowerOfThree(int n) {
+     bool isPowerOfThree(int n) {
         if(n<=0)
             return false;
-        while(n%3==0)
-            n=n/3;
-        return n==1;        
-    }
+        return((n==1) || (n%3==0 && isPowerOfThree(n/3)));
+     }
+
 };
