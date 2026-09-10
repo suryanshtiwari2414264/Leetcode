6c39db943e7a14e3f207c048673df0a4f9aad40c
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-09-10 10:21:07 +0530

    Time: 4 ms (36.69%), Space: 25.8 MB (23.5%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -9,41 +9,70 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    void dfs(TreeNode* root,vector<int>&arr){
+// class Solution {
+// private:
+//     void dfs(TreeNode* root,vector<int>&arr){
+//         if(root == nullptr)
+//             return;
+//         dfs(root->left,arr);
+//         arr.push_back(root->val);
+//         dfs(root->right,arr);
+//     }
+// public:
+//     vector<int> findMode(TreeNode* root) {
+//         vector<int> arr;
+//         dfs(root,arr);
+//         vector<int> ans;
+//         if(arr.empty())
+//             return ans;
+//         int count=1;
+//         int maxcount=1;
+//         ans.push_back(arr[0]);
+//         for(int i=1;i<arr.size();i++){
+//             if (arr[i] == arr[i - 1]) {
+//                 count++;
+//             }
+//             else {
+//                 count = 1;
+//             }
+//             if (count > maxcount) {
+//                 maxcount = count;
+//                 ans.clear();
+//                 ans.push_back(arr[i]);
+//             }
+//             else if (count == maxcount) {
+//                 ans.push_back(arr[i]);
+//             }
+//         }
+//         return ans;   
+//     }
+// };
+
+
+//USing MAP
+class Solution{
+private:
+    void inorder(TreeNode* root,map<int,int> &mp){
         if(root == nullptr)
             return;
-        dfs(root->left,arr);
-        arr.push_back(root->val);
-        dfs(root->right,arr);
+        inorder(root->left,mp);
+        mp[root->val]++;
+        inorder(root->right,mp);
     }
 public:
-    vector<int> findMode(TreeNode* root) {
-        vector<int> arr;
-        dfs(root,arr);
-        vector<int> ans;
-        if(arr.empty())
-            return ans;
-        int count=1;
-        int maxcount=1;
-        ans.push_back(arr[0]);
-        for(int i=1;i<arr.size();i++){
-            if (arr[i] == arr[i - 1]) {
-                count++;
-            }
-            else {
-                count = 1;
-            }
-            if (count > maxcount) {
-                maxcount = count;
-                ans.clear();
-                ans.push_back(arr[i]);
-            }
-            else if (count == maxcount) {
-                ans.push_back(arr[i]);
+    vector<int> findMode(TreeNode* root){
+        map<int,int>mp;
+        inorder(root,mp);
+        vector<int>ans;
+        int maxcount=0;
+        for(auto p:mp){
+            maxcount = max(maxcount,p.second);
+        }
+        for(auto p:mp){
+            if(p.second == maxcount){
+                ans.push_back(p.first);
             }
         }
-        return ans;   
+        return ans;
     }
 };
