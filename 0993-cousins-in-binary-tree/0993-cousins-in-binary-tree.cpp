436d19ba1361bbcca9d63184e423a0e34827d9e0
Suryansh Tiwari <suryanshtiwarisuraj@gmail.com>
2026-09-11 22:55:02 +0530

    Time: 0 ms (100%), Space: 14.3 MB (8.06%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -9,30 +9,84 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-private:
-TreeNode* XParent = NULL;
-TreeNode* YParent = NULL;
-int Xdepth = -1;
-int Ydepth = -1;
-void dfs(TreeNode* root,int x, int y,TreeNode* parent, int depth){
-    if(root == NULL)
-        return;
-    if(root->val==x)
-    {
-        XParent = parent;
-        Xdepth = depth;
-    }
-    if(root-> val == y){
-        YParent = parent;
-        Ydepth = depth;
-    }
-    dfs(root->left,x, y,root,depth+1);
-    dfs(root->right,x, y,root,depth+1);
-}
+ // DFS APPROACH
+// class Solution {
+// private:
+// TreeNode* XParent = NULL;
+// TreeNode* YParent = NULL;
+// int Xdepth = -1;
+// int Ydepth = -1;
+// void dfs(TreeNode* root,int x, int y,TreeNode* parent, int depth){
+//     if(root == NULL)
+//         return;
+//     if(root->val==x)
+//     {
+//         XParent = parent;
+//         Xdepth = depth;
+//     }
+//     if(root-> val == y){
+//         YParent = parent;
+//         Ydepth = depth;
+//     }
+//     dfs(root->left,x, y,root,depth+1);
+//     dfs(root->right,x, y,root,depth+1);
+// }
+// public:
+//     bool isCousins(TreeNode* root, int x, int y) {
+//         dfs(root,x,y,NULL,1);//Call 
+//         return (XParent != YParent && Xdepth == Ydepth);
+//     }
+// };
+// BFS APPROACH
+class Solution{
 public:
-    bool isCousins(TreeNode* root, int x, int y) {
-        dfs(root,x,y,NULL,1);//Call 
-        return (XParent != YParent && Xdepth == Ydepth);
+    bool isCousins(TreeNode* root, int x, int y){
+        if(root==nullptr)
+            return false;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            bool sibling = false;
+            bool cousins = false;
+            int lvl_size = q.size();
+            for(int i = 1; i<= lvl_size;i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node == NULL)
+                {
+                    sibling = false;
+                }
+                else
+                {
+                    if(node->val == x || node->val ==y)
+                    {
+                        if(cousins== false)
+                        {
+                            sibling = true;
+                            cousins = true;
+                        }
+                        else
+                        {
+                            if(sibling == false)
+                                return true;
+                            else
+                                return false;
+                        }
+
+                    }
+                    if(node->left!=NULL)
+                        q.push(node->left);
+                    if(node->right!= NULL)
+                        q.push(node->right);
+                    q.push(NULL);
+                }
+            }
+            if(cousins)
+                return false;
+
+        }
+        return false;
     }
 };
